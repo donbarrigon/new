a8ddabe309df471506f483e37dd772b2b58de93c
Don Barrigon <soydonbarrigon@gmail.com>
2026-05-01 00:50:33 -0500

    feat: nuevo main mas limpio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use forge::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut server = Server::new("127.0.0.1", 8080);
-    server.listen().await?;
-    Ok(())
+    forge::server_start().await?;
+    return Ok(())
 }
